58bdd2022f2e6b3c3c0cfa4011080dbf97323bde
Adu-Sarpong <125971979+Adu-Sarpong@users.noreply.github.com>
2023-09-21 18:53:53 +0000

    uodated portfolio

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
+
 import About from '@/components/About';
 import Contact from '@/components/Contact';
 import Experience from '@/components/Experience';
-import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 import Projects from '@/components/Projects';
 import Socials from '@/components/Socials';
