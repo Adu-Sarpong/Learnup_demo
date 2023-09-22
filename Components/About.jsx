de39fbe0f82e39d7f2187b9d9da87f6e3c2065a9
Adu-Sarpong <125971979+Adu-Sarpong@users.noreply.github.com>
2023-09-22 09:44:53 +0000

    made changes

--- Components/About.jsx
+++ Components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div id='about' className='h-[70vh] w-full bg-violet-400 text-4xl flex justify-center items-center'>About</div>
+    <div id='about' className='h-[70vh] w-full bg-violet-400 text-4xl flex justify-center items-center'>About in Progress</div>
   )
 }
 export default About
