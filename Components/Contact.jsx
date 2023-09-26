bc5b9cdebb965e8f706bcb4018c5b6163d9168d3
Adu-Sarpong <125971979+Adu-Sarpong@users.noreply.github.com>
2023-09-26 11:59:36 +0000

    working on updates

--- Components/Contact.jsx
+++ Components/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div id='contact' className='h-[70vh] w-full bg-violet-400 text-4xl flex justify-center items-center'>Contact
+    <div id='contact' className='h-[70vh] w-full bg-violet-400 text-4xl flex justify-center items-center'>Contact is in progress
     </div>
   )
 }
